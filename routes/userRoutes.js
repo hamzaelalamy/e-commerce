9383accc906e52136a3a82f2fe6715994b84cde5
yhy-mouli <115432261+yhy-mouli@users.noreply.github.com>
2024-03-06 12:49:19 +0100

    deleting commented part from userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,13 +11,5 @@ router.patch('/:id', userController.modify);
 
 router.get('/:id', userController.singleUser);
 
-// router.post('/create', [
-//     body('name').notEmpty().isString(),
-//     body('email').notEmpty().isString(),
-//     body('number').notEmpty().isNumeric(),
-//     body('role').notEmpty().isString(),
-//     body('password').notEmpty().isString()
-// ], validateRequest, "Smail-Smail-CreateUser.create");
-
 
 module.exports = router;
