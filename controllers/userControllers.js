1a2e14e24736a4f2ba3b08fdd58e5c30c14e5fdb
unknown <hamza.elalamy1@uit.ac.ma>
2024-03-10 12:55:10 +0000

    fix

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,21 +1,21 @@
 const User = require('../models/users');
-require('dotenv').config();
+
 
 module.exports.getAllUsers = async (req, res) => {
     try {
-        
+        // Retrieve all users from the database
         const users = await User.find({});
 
-
+        // Check if there are no users found
         if (!users || users.length === 0) {
             return res.status(404).json({ message: 'No users found' });
         }
 
-
+        // Return the retrieved users as a response
         res.status(200).json(users);
     } catch (error) {
         console.error(error);
-        next(err);
+        res.status(500).json({ message: 'Internal server error' });
     }
 };
 
@@ -27,7 +27,7 @@ module.exports.updateUser = async (req, res, next) => {
         const updatedUser = await User.findByIdAndUpdate(id, updateInfo, { new: true });
         if (updatedUser) {
             console.log("Updated user successfully");
-            res.status(200).json({ message: "Update successful!", data: updatedUser });
+            res.status(200).json({ message: "Update successfully!", data: updatedUser });
         } else {
             res.status(404).json({ message: "No such user is present or the fields you are trying to update are incorrect." });
         }
@@ -36,7 +36,7 @@ module.exports.updateUser = async (req, res, next) => {
     }
 };
 
-module.exports.delete = async (req, res, next) => {
+module.exports.deleteUser = async (req, res, next) => {
     const id = req.params.id;
     try {
         await User.findByIdAndDelete(id);
@@ -47,47 +47,14 @@ module.exports.delete = async (req, res, next) => {
     }
 };
 
-module.exports.user = async (req, res, next) =>{
-    try{
-        const ShowUsers = await User.find({});
-        console.log('Users Stored: ', ShowUsers);
-        res.status(201).json(ShowUsers);
-    }catch (err) {
-        console.log('Problem in found users');
-        next(err);
-    }
-};
-
-
-module.exports.modify = async (req, res, next) => {
-    const id = req.params.id;
-    const updateInfo = req.body;
-    try {
-        const patchedUser = await User.findByIdAndUpdate(id, updateInfo, { new: true });
-        if(patchedUser) {
-            console.log(id, "is patched");
-            res.status(200).json({message: 'Patched successfully: ', data: patchedUser});
-        } else {
-            console.log("The user was not found");
-            res.status(404).json({ message: "The user you are looking for doesn't exist."});
-        }
-    } catch (err){
-        next(err);
-    }   
-       
-};
-
-
-module.exports.singleUser = async (req, res, next) => {
+module.exports.getSingleUser = async (req, res, next) => {
     const id = req.params.id;
     console.log(id);
-    try{
-        const suser = await User.findById(id);
-        console.log(suser);
-        if(!suser)
+    try {
+        const singleUser = await User.findById(id);
+        if (!singleUser)
             return res.status(404).json({ message: 'No user with this ID was found' });
-        return res.status(200).json(suser);
-    }catch (err) {
+        return res.status(200).json(singleUser);
+    } catch (err) {
         next(err);
-    }
-};
+    }};
